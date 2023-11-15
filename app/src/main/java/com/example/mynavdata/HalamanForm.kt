b490b7918c5a41d2fbeffb94b4ef51e6c1ee1b59
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2023-11-15 18:28:31 +0700

    menambahkan string dan membuat function HalamanForm

--- app/src/main/java/com/example/mynavdata/HalamanForm.kt
+++ app/src/main/java/com/example/mynavdata/HalamanForm.kt
@@ -1,2 +1,75 @@
 package com.example.mynavdata
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HalamanForm(
+    onSubmitButtonClicked: (MutableList<String>) -> Unit) {
+
+    var namaTxt by remember {
+        mutableStateOf("")
+    }
+    var alamatTxt by remember {
+        mutableStateOf("")
+    }
+    var tlpnTxt by remember {
+        mutableStateOf("")
+    }
+    var listData: MutableList<String> = mutableListOf(namaTxt,alamatTxt,tlpnTxt)
+
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        OutlinedTextField(
+            value = namaTxt,
+            onValueChange = {
+                namaTxt = it
+            },
+            label = { Text(text = stringResource(id = R.string.nama)) }
+        )
+
+        OutlinedTextField(
+            value = alamatTxt,
+            onValueChange = {
+                alamatTxt = it
+            },
+            label = { Text(text = stringResource(id = R.string.alamat)) }
+        )
+
+        OutlinedTextField(
+            value = tlpnTxt,
+            onValueChange = {
+                tlpnTxt = it
+            },
+            label = { Text(text = stringResource(id = R.string.telepon)) }
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        Button(onClick = { onSubmitButtonClicked(listData) }) {
+            Text(text = stringResource(id = R.string.btn_submit))
+        }
+
+    }
+}
